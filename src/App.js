be221ea1c18c9ed9e30fb31569e20ff10920e34d
Shean <ys.chong@finexusgroup.com>
2018-02-05 18:56:24 +0800

    Functional app without styling.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
 // @flow
+import React from "react";
+import AddOption from "./components/AddOption";
+import Action from "./components/Action";
+import Header from "./components/Header";
+import Options from "./components/Options";
+import OptionModal from "./components/OptionModal";
 
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+export default class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
+    this.handleClearSelectedOption = this.handleClearSelectedOption.bind(this);
+    this.state = {
+      options: [],
+      selectedOption: undefined
+    };
+  }
+  handleDeleteOptions() {
+    this.setState(() => ({ options: [] }));
+  }
+  handleClearSelectedOption() {
+    this.setState(() => ({ selectedOption: undefined }));
+  }
+  handleDeleteOption(optionToRemove) {
+    this.setState(prevState => ({
+      options: prevState.options.filter(option => optionToRemove !== option)
+    }));
+  }
+  handlePick() {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    this.setState(() => ({
+      selectedOption: option
+    }));
+  }
+  handleAddOption(option) {
+    if (!option) {
+      return "Enter valid value to add item";
+    } else if (this.state.options.indexOf(option) > -1) {
+      return "This option already exists";
+    }
 
-class App extends Component {
+    this.setState(prevState => ({
+      options: prevState.options.concat(option)
+    }));
+  }
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem("options");
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({ options }));
+      }
+    } catch (e) {
+      // Do nothing at all
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem("options", json);
+    }
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
   render() {
+    const subtitle = "Put your life in the hands of a computer";
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header subtitle={subtitle} />
+        <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        <AddOption
+          handleAddOption={this.handleAddOption}
+        />
+        <OptionModal
+          selectedOption={this.state.selectedOption}
+          handleClearSelectedOption={this.handleClearSelectedOption}
+        />
       </div>
     );
   }
 }
-
-export default App;
